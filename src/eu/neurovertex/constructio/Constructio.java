4388f951e1ed9e3ceb2ea8f0a781a92db4fcaa11
NeuroVertex <nv@neurovertex.eu>
2014-09-30 14:41:56 +0200

    Initial commit

---
+++ src/eu/neurovertex/constructio/Constructio.java
@@ -0,0 +1,11 @@
+package eu.neurovertex.constructio;
+
+/**
+ * @author Neurovertex
+ *         Date: 27/09/2014, 18:57
+ */
+public class Constructio {
+	public static void main(String[] args) {
+
+	}
+}
